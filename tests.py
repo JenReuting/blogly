02327447a0a3f65533feca93712ef01103ef087b
Christopher Lim <cli3338198@gmail.com>
2022-10-20 12:33:58 -0700

    test routes

--- tests.py
+++ tests.py
@@ -92,15 +92,71 @@ class UserViewTestCase(TestCase):
                     "image-url": 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/fb-master-template-2-47-1659701917.jpg'},
                     follow_redirects=True)
 
-            # html = resp.get_data(as_text=True)
-
+            # test that user is in the html
             html = resp.get_data(as_text=True)
-
-            print(f'<<<<<<<_____________########  {html}')
-
             self.assertIn("Jennifer Gates", html)
+            # test that this newly created user is in the db
+            users = User.query.filter_by(first_name = "Jennifer").all()
+            self.assertEqual(len(users), 1)
+            # test that this newly created user is not in the db
+            users = User.query.filter_by(first_name = "Jenniferzdadsasf").all()
+            self.assertNotEqual(len(users), 1)
+            # test thatt some random user does not exist in the db
+            fake_users = User.query.filter_by(first_name = "Elon").all()
+            self.assertNotEqual(len(fake_users), 1)
 
-            # db.session.delete(test_user)
-            # db.session.commit()
+
+    def test_edit_user(self):
+        """Test the edit user route"""
+        with self.client as c:
+            c.post('/users/new',
+                data={"first-name": 'Chris',
+                    "last-name": 'Lim',
+                    "image-url": 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/fb-master-template-2-47-1659701917.jpg'},
+                    follow_redirects=True)
+
+            users = User.query.filter_by(first_name = "Chris").all()
+            user = users[0]
+            user_id = user.id
+
+            # check that get redirect to /users
+            # check that edited user is in the /users
+            resp = c.post(f'/users/{user_id}/edit', 
+                        data={"first-name": "CHRIS", "last-name": "LIM"}, 
+                        follow_redirects=True)
+            users_list_html = resp.get_data(as_text=True)
+            self.assertIn("CHRIS LIM", users_list_html)
+            self.assertNotIn('Chris Lim', users_list_html)
+
+            # check that edited user page is updated
+            user_detail_resp = c.get(f'/users/{user_id}')
+            user_detail_html = user_detail_resp.get_data(as_text=True)
+            self.assertIn('CHRIS LIM', user_detail_html)
+            self.assertNotIn('Chris Lim', user_detail_html)
+
+    def test_delete_user(self):
+        """Test the delete user route"""
+        with self.client as c:
+
+            users = User.query.filter_by(first_name = "test_first").all()
+            test_user = users[0]
+            test_user_id = test_user.id
+
+            c.post(f'/users/{test_user_id}/delete', follow_redirects=True)
+
+            # check user not in db
+            users = User.query.filter_by(first_name = "test_first").all()
+            self.assertEqual(len(users), 0)
+
+            # check the deleted user detail page
+            resp = c.get(f'/users/{test_user_id}')
+            self.assertEqual(resp.status_code, 404)
+            
+
+            
+
+
+
+
 
 
